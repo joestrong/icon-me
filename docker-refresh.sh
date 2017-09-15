4e8ba288ecb82d69e94e9052e51ab72552d3d512
Joe Strong <joestrong@gmail.com>
2017-09-15 22:56:57 +0100

    Renamed project to icon.me

--- docker-refresh.sh
+++ docker-refresh.sh
@@ -1,7 +1,7 @@
 echo "Copying repo"
-docker run --rm --volumes-from=logo-me-data -v $(pwd):/source debian:jessie cp -r /source/. /web/
+docker run --rm --volumes-from=icon.me-data -v $(pwd):/source debian:jessie cp -r /source/. /web/
 
 echo "Setting permissions"
-docker run --rm --volumes-from=logo-me-data -w /web debian:jessie chmod -R 755 .
+docker run --rm --volumes-from=icon.me-data -w /web debian:jessie chmod -R 755 .
 
 echo "Refreshed!"
